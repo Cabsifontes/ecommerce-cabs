2150ba2e70f80c59771ce28f49cc0183408a754b
Cabs <cabs.sifontes@gmail.com>
2025-03-31 13:27:38 -0300

    Cambios rama feature/pages

--- src/pages/AdminCreateUpdateProduct.jsx
+++ src/pages/AdminCreateUpdateProduct.jsx
@@ -1,19 +1,15 @@
-import { useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import FormAdmin from "../components/form/FormAdmin";
 
 const AdminCreateUpdateProduct = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const id = queryParams.get("id");
-  const isEditMode = id !== null;
+  const idParams = new URLSearchParams(location.search).get("id");
   return (
     <>
-      <h2 className="text-center mt-5" style={{ color: "#6e6e6c" }}>
-        {isEditMode ? "Editar Producto" : "Crear Producto"}
+      <h2 className="text-center mt-5 mb-5" style={{ color: "#6e6e6c" }}>
+        {idParams ? "Editar Producto" : "Crear Producto"}
       </h2>
-      <Container className="d-flex justify-content-center my-3">
-        <FormAdmin />
+      <Container className="d-flex justify-content-center aling-items-center vh-100">
+        <FormAdmin/>
       </Container>
     </>
   );
